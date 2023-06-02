be9ba629bfe334cec95cb07619730a744a51ad79
JiahuiQue <118904915+JiahuiQue@users.noreply.github.com>
2023-06-02 09:27:49 +0200

    Update drv_infrared.cpp
    Added function irRead

--- lib/drv_infrared/drv_infrared.cpp
+++ lib/drv_infrared/drv_infrared.cpp
@@ -1,1 +1,20 @@
 #include <Arduino.h>
+
+int irRead(int cutoff1, int cutoff2, pinData& sensor) {
+  int analogValue = analogRead(sensor.sensorPin);                     // Read the value from the analog channel
+
+  sensor.total -= sensor.readings[sensor.readIndex];                   // Subtract the last reading
+  sensor.readings[sensor.readIndex] = analogValue;                     // Store the new reading
+  sensor.total += sensor.readings[sensor.readIndex];                   // Add the new reading to the total
+  sensor.readIndex = (sensor.readIndex + 1) % numReadings;             // Move to the next position in the array
+
+  int count = max(sensor.readIndex + 1, numReadings);                  // Number of valid readings
+  sensor.average = sensor.total / count;                               // Calculate the rolling average
+
+  if (sensor.average > cutoff1)      
+    return 1;
+  else if (sensor.average < cutoff2) 
+    return 2;
+  else                        
+    return 0;
+}
