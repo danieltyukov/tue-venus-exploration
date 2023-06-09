cc29e9f897c967e121271d108c3f082cdfe41cc0
malikweren <malikweren@gmail.com>
2023-06-09 11:01:57 +0200

    updates to ultrasonic

--- lib/drv_ultrasonic/drv_ultrasonic.hpp
+++ lib/drv_ultrasonic/drv_ultrasonic.hpp
@@ -7,5 +7,6 @@
 const int pingPin = 9;
 
 bool detectObstacle();
+void detect_mountain(bool check_forward_mountain,bool check_left_mountain,bool check_right_mountain,int pin);
 
 #endif  // DRV_ULTRASONIC_H
