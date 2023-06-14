55840b664d2b6bc1bb79e5cea506eae31ec8ab5e
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2023-06-14 15:13:34 +0200

    small lab update

--- lib/drv_labratory/drv_labratory.cpp
+++ lib/drv_labratory/drv_labratory.cpp
@@ -4,7 +4,7 @@ bool checkLabDetected(int labratory_pin) {
     int j = 0;
     for (int i = 0; i < 40; i++) {
         j+=digitalRead(labratory_pin);
-        delay(5);
+        delay(20);
     }
     int avg = j/40;
     return !avg;
